7ed3fd8f63e643a70cc1f87b32e46ea01ff45c5f
rodrigo0345 <rodrigocralha@gmail.com>
2024-07-06 15:38:23 +0100

    feat: Window creation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,78 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
 
+bool is_running = true;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+bool initialize_window(void) {
+  // creating a window
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
+
+  window =
+      SDL_CreateWindow("Renderer", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+
+  if (!window) {
+    fprintf(stderr, "Error creating SDL Window");
+    return false;
+  }
+
+  // -1 tells sdl to get the default graphics
+  renderer = SDL_CreateRenderer(window, -1, 0);
+
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL Renderer");
+    return false;
+  }
+
+  return true;
+}
+
+void setup(void) {}
+
+void process_input(void) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+
+  switch (event.type) {
+  case SDL_QUIT:
+    is_running = false;
+    break;
+  case SDL_KEYDOWN:
+    if (event.key.keysym.sym == SDLK_ESCAPE) {
+      is_running = false;
+    }
+    break;
+  }
+}
+
+void update(void) {}
+
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+}
+
 int main(void) {
-  printf("Hello there\n");
+  is_running = initialize_window();
+
+  setup();
+  while (is_running) {
+    process_input();
+    update();
+    render();
+  }
+
   return 0;
 }
